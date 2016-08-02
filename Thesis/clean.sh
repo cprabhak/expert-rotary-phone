c624ff5baf8cb9a65e1dc005a1dad986e776b0b9
cprabhak <cprabhakar@gmail.com>
2016-08-02 21:04:34 +0800

    Initial upload

---
+++ Thesis/clean.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+mkdir old
+mv *.aux old
+mv *.bbl old
+mv *.bcf old
+mv *.blg old
+mv *.dvi old
+mv *.glo old
+mv *.ilg old
+mv *.ind old
+mv *.lof old
+mv *.log.txt old
+mv *.lot old
+mv *.nlo old
+mv *.out old
+mv *.pdf old
+mv *.xml old
+mv *.gz old
+mv *.toc old
+mv *.nls old
